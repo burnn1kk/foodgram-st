b740d78fb550d9ef8174fc5aa3212b39271642cf
burnn1kvm <sigmahackerkiller@example.com>
2025-11-18 16:20:11 +0000

    reqirements.txt

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%c_x3amuz=1ihect135#q8yrvpzs1(tzsmb=!*7rc1fmqwizq$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0','192.168.1.58']
 
 
 # Application definition
