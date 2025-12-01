505839526c7fe484d8c595176a90f426fb69d8b6
burnn1kvm <burnn1kvm@UbuntuServer@gmail.com>
2025-12-01 16:33:30 +0000

    deployed nahuy

---
+++ backend/foodgram/import_ingredients.py
@@ -0,0 +1,21 @@
+import csv
+import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodgram.settings")
+django.setup()
+
+from posts.models import Ingredient  # твоя модель
+
+with open("ingredients.csv", encoding="utf-8") as f:
+    reader = csv.reader(f)  # просто reader без заголовков
+    for row in reader:
+        name = row[0].strip()
+        measurement_unit = row[1].strip()
+        Ingredient.objects.get_or_create(
+            name=name,
+            measurement_unit=measurement_unit
+        )
+
+print("Ingredients imported successfully!")
+
