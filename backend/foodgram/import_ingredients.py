d3d7225dfb2584bcf5ed08edd35a2da1644e26cb
burnn1k <fsociety@example.com>
2025-12-04 16:26:27 +0300

    ruff format

--- backend/foodgram/import_ingredients.py
+++ backend/foodgram/import_ingredients.py
@@ -1,3 +1,4 @@
+# Скрипт для загрузки рецептов в БД
 import csv
 import os
 import django
@@ -5,17 +6,13 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "foodgram.settings")
 django.setup()
 
-from posts.models import Ingredient  # твоя модель
+from posts.models import Ingredient
 
 with open("ingredients.csv", encoding="utf-8") as f:
-    reader = csv.reader(f)  # просто reader без заголовков
+    reader = csv.reader(f)
     for row in reader:
         name = row[0].strip()
         measurement_unit = row[1].strip()
-        Ingredient.objects.get_or_create(
-            name=name,
-            measurement_unit=measurement_unit
-        )
+        Ingredient.objects.get_or_create(name=name, measurement_unit=measurement_unit)
 
 print("Ingredients imported successfully!")
-
